8476a9fbd921fce6f8ebb10f6ed68df41fb90272
Ogundare Oluwafemi <ogundarearnold@gmail.com>
2025-03-14 19:50:31 +0100

    Resumed Practice and learnt to use the Super Keyword

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // Super = Keyword refers to the superclass (parent) of an object
+        //         very similar to the "this" keyword
+
+        Hero hero = new Hero("Femi", 38, "Super Strength");
+        Hero her1 = new Hero("Oluwafemi", 39, "Super Strength");
+
+        hero.age = 40;
+        hero.power = "Super Speed";
+        hero.name = "Samantha";
+        System.out.println(hero.name);
+
+        System.out.println(her1.toString());
+
+
+    }
+}
